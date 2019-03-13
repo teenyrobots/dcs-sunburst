27067a02a78bb5a5e07327985488eb3472896bfd
Joyce <hellojoycerice@gmail.com>
2019-03-13 14:36:05 -0400

    sections inner circle and makes titles show up on click

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 // SOLUTIONS MATRIX D3 SUNBURST EDITION
 
+// colors
+let colors1 = [
+  "red",
+  "blue",
+  "green"
+]
+
 // this function takes the data and ports it into the function that generates the viz
 d3.json("data.json", function(data) {
   theData = data;
@@ -12,13 +19,32 @@ d3.json("data.json", function(data) {
 
 // helper functions
 function arrayify(theData) {
-  let newArray = []
+  let newArray = [];
   for (var i in theData) {
     let newDatum = theData[i].interventions.length;
     newArray.push(newDatum);
   }
-  console.log(newArray);
   return newArray;
+}
+
+function innerArrayContent(theData) {
+  let newArray = [];
+  for (var i in theData) {
+    for (j in theData[i].interventions) {
+      let newDatum = theData[i].interventions[j];
+      newArray.push(newDatum);
+    }
+  }
+  return newArray;
+};
+
+function innerArray(theData) {
+  let newArray = innerArrayContent(theData)
+  let numericArray = [];
+  for (i in newArray) {
+    numericArray.push(newArray.length / newArray.length);
+  }
+  return numericArray;
 }
 
 //
@@ -35,10 +61,13 @@ function sunburst(id){
 
   // just circle stuff
   let outerRadius = w / 2,
-	 innerRadius = h / 5,
+	 innerRadius = h * .25,
    arc = d3.arc()
     .innerRadius(innerRadius)
-    .outerRadius(outerRadius);
+    .outerRadius(outerRadius),
+  innerArc = d3.arc()
+     .innerRadius(innerRadius * .8)
+     .outerRadius(outerRadius * .8);
 
   let pie = d3.pie();
 
@@ -56,9 +85,43 @@ function sunburst(id){
       .attr("transform", "translate("+ outerRadius + "," + outerRadius +")");
 
     arcs.append("path")
-      .attr("fill", "#ff5572")
-      .attr("stroke", "white")
+      .attr("fill", function(d, i) {
+        return colors1[i];
+      })
       .attr("d", arc);
+
+  console.log(innerArrayContent(theData));
+
+  let innerArcs = svg.selectAll("g.innerArc")
+      .data(pie(innerArray(theData)))
+      .enter()
+      .append('g')
+      .attr("transform", "translate("+ outerRadius + "," + outerRadius +")");
+
+    innerArcs.append("path")
+      .attr("fill", function(d, i) {
+        if(i < 10) {
+          let lilColor = "rgb(255,"+ i*10 +", 255)";
+          console.log(lilColor);
+          return lilColor;
+        } else if (i < 20){
+          let lilColor = "rgb(255, 255,"+ i*10 +")";
+          console.log(lilColor);
+          return lilColor;
+        } else {
+          return "#000";
+        }
+      })
+      .attr("stroke", "white")
+      .attr("d", innerArc)
+      .on("click", function(d, i) {
+        svg.append("text")
+          .text(innerArrayContent(theData)[i])
+          .attr("x", w/2)
+          .attr("y", h/2)
+          .style("text-anchor", "middle");
+        console.log(innerArrayContent(theData)[i]);
+    });
 
 
   // print all the titles
